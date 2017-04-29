ba9a1fed9c85cc2b744058c0f8139e8dec667f1b
Daryl McMillan <darylm+github@gmail.com>
2017-04-29 16:48:51 -0400

    hard sync works!

--- main.cc
+++ main.cc
@@ -3,7 +3,7 @@
 
 // 0-1023
 unsigned short analogRead( char pin ) {
-  ADMUX = (0 << REFS0) | (1 << ADLAR) | (0b10 << MUX0);
+  ADMUX = (0 << REFS0) | (1 << ADLAR) | (pin << MUX0);
   ADCSRA = (1 << ADEN) | (1 << ADSC) | (0b110 << ADPS0);
   while( ADCSRA & (1 << ADSC) );
   return ADC;
@@ -17,19 +17,39 @@ void setPin( char pin, char value ) {
   PORTB = ( PORTB & ~(1<<pin) ) | (value<<pin);
 }
 
+void setInput( char pin ) {
+  DDRB &= ~( 1 << pin );
+  PORTB &= ~( 1 << pin );
+}
+
+unsigned char getPin( char pin ) {
+  return ( PINB >> pin ) & 1;
+}
+
 int main(void) {
+
+  // comparator setup
+  ADCSRB &= ~( 1 << ACME ); // no multiplex
+  ACSR = 3 << ACIS0; // detect rising edge
+  DIDR0 |= 0b11; // disable digital
 
   unsigned char mux = (0 << REFS0) | (1 << ADLAR) | (0b10 << MUX0);
   unsigned char start = (1 << ADEN) | (1 << ADSC) | (0b110 << ADPS0);
 
-  unsigned long step = 0x4FFFF;
+  unsigned long step = 0x0FFFF;
   unsigned long duty = 32000;
 
-  setOutput( 3 );
+  setOutput( 2 );
 
   unsigned long tick = 0;
 
   for( ;; ) {
+
+    if( ACSR & ( 1 << ACI ) ) {
+      tick = 0;
+      ACSR |= 1 << ACI; // clear by writing 1 (!)
+    }
+
     if( ADCSRA & ( 1 << ADSC ) ) {
       
     } else {
@@ -42,9 +62,9 @@ int main(void) {
     }
     tick += step;
     if( tick < duty ) {
-      setPin( 3, 1 );
+      setPin( 2, 1 );
     } else {
-      setPin( 3, 0 );
+      setPin( 2, 0 );
     }
   }
 
