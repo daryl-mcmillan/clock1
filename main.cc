3f7c63f9488a16129aba44c9280edd6e81ae9287
Daryl McMillan <darylm+github@gmail.com>
2017-05-04 22:26:16 -0400

    frequency control

--- main.cc
+++ main.cc
@@ -2,14 +2,6 @@
 #include <util/delay.h>
 #include "adc.h"
 
-// 0-1023
-unsigned short analogRead( char pin ) {
-  ADMUX = (0 << REFS0) | (1 << ADLAR) | (pin << MUX0);
-  ADCSRA = (1 << ADEN) | (1 << ADSC) | (0b110 << ADPS0);
-  while( ADCSRA & (1 << ADSC) );
-  return ADC;
-}
-
 void setOutput( char pin ) {
   DDRB |= ( 1 << pin );
 }
@@ -32,10 +24,6 @@ int main(void) {
   // comparator setup
   ADCSRB &= ~( 1 << ACME ); // no multiplex
   ACSR = ( 3 << ACIS0 ) | ( 1 << ACBG ); // detect rising edge, bandgap as positive input
-  // DIDR0 |= 0b11; // disable digital
-
-  //unsigned char mux = (0 << REFS0) | (1 << ADLAR) | (0b10 << MUX0);
-  //unsigned char start = (1 << ADEN) | (1 << ADSC) | (0b110 << ADPS0);
 
   unsigned long step = 0x0FFFF;
   unsigned long duty = 32000;
@@ -47,10 +35,12 @@ int main(void) {
   for( ;; ) {
 
     adc_refresh();
-    step = 0xFFFF;
+    step = adc_read( 3 );
+    step = step << 4;
     duty = adc_read( 2 );
     duty = duty << 16;
 
+    // hard sync
     if( ACSR & ( 1 << ACI ) ) {
       tick = 0;
       ACSR |= 1 << ACI; // clear by writing 1 (!)
